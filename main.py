7f483e3e3463608f6d321b8dabd9d97ff8db1797
satvik314 <113601830+satvik314@users.noreply.github.com>
2025-02-01 14:25:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ tweet_chain = tweet_prompt | gemini_model
 
 import streamlit as st
 
-st.header("Tweet Generator")
+st.header("🐦 Tweet Generator")
 
-st.subheader("Generate tweets using Generative AI")
+st.subheader("Generate tweets using Generative AI 🤖")
 
 topic = st.text_input("Topic")
 
